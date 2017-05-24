3f5abe1bf847d2aed639078ad5a2f1a887c16706
Kingshuk <afkingshuk@gmail.com>
2017-05-24 12:56:06 +0600

    Add files via upload

---
+++ Academic/CSE111_lab/Lab2/lab2_bonus.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class lab2_bonus
+{
+  public static void main(String[] args)
+  {
+    Scanner scanf = new Scanner(System.in);
+    int[] array = new int[5];
+    
+    for(int i=0; i<array.length ; i++)
+    {
+      array[i] = scanf.nextInt();
+      if((Math.sqrt(array[i])%2) == 0)
+      {
+        
+      }
+    }
+    
+  }
+}
